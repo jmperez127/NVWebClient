3429da0b084932845b61ab35fb2e03f0da1f9feb
Jean <jmperez127@gmail.com>
2017-01-18 02:58:17 -0400

    Refactor event subscriber method

--- js/app/app.js
+++ js/app/app.js
@@ -8,70 +8,66 @@ NVApp = {
   }
 };
 
-var Bootstrapper = function () {
-  var ObserverSubject = function () {
-    var self = this;
-    this.observers = [];
-
-    return {
-      subscribe: function (observer) {
-        self.observers.push(observer);
-      },
-      unsubscribe: function (observer) {
-        var index = self.observers.indexOf(observer);
-        if (index > -1) self.observers.splice(index, 1);
-
-      },
-      notify: function (observer, event, args) {
-        var index = self.observers.indexOf(observer);
-        if (index > -1) observer.notify(event, args);
-      },
-      notifyAll: function () {
-        for (var i = 0; i < self.observers.length; i++) {
-          self.observers[i].notify(i);
-        }
-        ;
-      }
-    };
-  };
-
-  var Observer = function (callback) {
-    return {
-      notify: function (event, args) {
-        callback(event, args);
-      }
-    }
-  };
-
-  var appObservers = {};
-  var subject = new ObserverSubject();
-
-  var onEvent = function (event, callback) {
-    appObservers[event] = new Observer(callback);
-    subject.subscribe(appObservers[event]);
-    return this;
-  };
-
-  var emitEvent = function (event, args) {
-    subject.notify(appObservers[event], event, args);
-  };
-
-  var unsubscribeEvent = function (event, callback) {
-    subject.unsubscribe(appObservers[event]);
-    delete appObservers[event];
-    if (typeof callback !== undefined)
-      callback(event);
-
-  };
+var BootstrapApp = function () {
+  observer = new ObserverSubscribtionManager();
 
   return function (func) {
-    this.on = onEvent;
-    this.emit = emitEvent;
-    this.unsubscribe = unsubscribeEvent;
+    this.on = observer.onEvent;
+    this.emit = observer.emitEvent;
+    this.unsubscribe = observer.unsubscribeEvent;
     NVApp.codeBlocks.push(func);
+  };
+
+  // Event subscriber / observer implementation
+  function ObserverSubscribtionManager() {
+    var Subject = function () {
+        var self = this;
+        this.observers = [];
+
+        return {
+          subscribe: function (observer) {
+            self.observers.push(observer);
+          },
+          unsubscribe: function (observer) {
+            var index = self.observers.indexOf(observer);
+            if (index > -1) self.observers.splice(index, 1);
+
+          },
+          notify: function (observer, event, args) {
+            var index = self.observers.indexOf(observer);
+            if (index > -1) observer.notify(event, args);
+          }
+        };
+      },
+      Observer = function (callback) {
+        return {
+          notify: function (event, args) {
+            callback(event, args);
+          }
+        }
+      },
+      appObservers = {},
+      subject = new Subject();
+
+    this.onEvent = function (event, callback) {
+      appObservers[event] = new Observer(callback);
+      subject.subscribe(appObservers[event]);
+      return this;
+    };
+
+    this.emitEvent = function (event, args) {
+      subject.notify(appObservers[event], event, args);
+    };
+
+    this.unsubscribeEvent = function (event, callback) {
+      subject.unsubscribe(appObservers[event]);
+      delete appObservers[event];
+      if (typeof callback !== undefined)
+        callback(event);
+    };
   }
 };
 
-NV = new Bootstrapper();
+NV = new BootstrapApp();
 
 
